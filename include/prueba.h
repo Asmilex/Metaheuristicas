16465d5601ebd5f78f9e676a87cc286afc17fd0b
Asmilex <andresmm@outlook.com>
2021-02-28 20:07:55 +0100

    Cambio a Rust

--- include/prueba.h
+++
@@ -1,4 +0,0 @@
-static int Factorial( int number ) {
-    //return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
-    return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
-}
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "prueba.h"
-
-using namespace std;
-
-int main(int argc, char const *argv[]) {
-    cout << "Hello world" << endl;
-}
