58de000bf7a7e2c6f168a8751381ffb78c23b01e
Asmilex <andresmm@outlook.com>
2021-03-12 00:24:18 +0100

    Implementados enumerados con los datos del problema

--- src/utils.rs
+++ src/utils.rs
@@ -1,17 +1,56 @@
 use nalgebra::*;
 
-pub type Punto = DVector<f64>;
-pub type MatrizDinamica<Tipo> = MatrixMN<Tipo, Dynamic, Dynamic>;
-pub type Restriccion = (Punto, Punto);
+//
+// ───────────────────────────────────────────────────── TIPOS PERSONALIZADOS ─────
+//
 
-pub fn distancia(p1: &Punto, p2: &Punto) -> f64 {
-    assert_eq!(p1.len(), p2.len());
 
-    let mut sum_cuadrados: f64 = 0.0;
+    pub type Punto = DVector<f64>;
+    pub type MatrizDinamica<Tipo> = MatrixMN<Tipo, Dynamic, Dynamic>;
+    pub type Restriccion = (Punto, Punto);
 
-    for i in 0..p1.len() {
-        sum_cuadrados += (p1[i] - p2[i]) * (p1[i] - p2[i]);
+
+//
+// ────────────────────────────────────────────────────── ENUMS Y ESTRUCTURAS ─────
+//
+
+
+    pub enum PAR_nombres {
+        Bupa,
+        Glass,
+        Zoo
     }
 
-    sum_cuadrados.sqrt()
-}
+    pub struct PAR_parametros {
+        pub tipo: PAR_nombres,
+        pub atributos: usize,
+        pub clusters: usize,
+        pub instancias: usize
+    }
+
+    impl PAR_parametros {
+        pub fn new (tipo: PAR_nombres) -> PAR_parametros {
+            match tipo {
+                PAR_nombres::Bupa  => PAR_parametros {tipo, atributos: 5,  clusters: 16, instancias: 345},
+                PAR_nombres::Glass => PAR_parametros {tipo, atributos: 9,  clusters: 7,  instancias: 214},
+                PAR_nombres::Zoo   => PAR_parametros {tipo, atributos: 16, clusters: 7,  instancias: 101}
+            }
+        }
+    }
+
+//
+// ──────────────────────────────────────────────────────────────── FUNCIONES ─────
+//
+
+
+    pub fn distancia(p1: &Punto, p2: &Punto) -> f64 {
+        assert_eq!(p1.len(), p2.len());
+
+        let mut sum_cuadrados: f64 = 0.0;
+
+        for i in 0 .. p1.len() {
+            sum_cuadrados += (p1[i] - p2[i]) * (p1[i] - p2[i]);
+        }
+
+        sum_cuadrados.sqrt()
+    }
