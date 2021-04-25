327334a085097a5a2953ca239de24c6f2f2e71d9
Asmilex <andresmm@outlook.com>
2021-04-25 18:21:57 +0200

    Implementado el operador de segmento fijo y cambiados los parámetros de ambos operadores

--- src/operator.rs
+++ src/operator.rs
@@ -1,14 +1,25 @@
+use rand::{Rng, rngs::StdRng};
+use std::cmp::{min, max};
+
+
 #[derive(Debug)]
 pub enum Operadores {
     Uniforme,
     SegmentoFijo
 }
 
-pub fn cruce_uniforme(padre_1: &Vec<usize>, padre_2: &Vec<usize>, cromosomas_padre_1: &Vec<usize>) -> Vec<usize> {
+pub fn cruce_uniforme(padre_1: &Vec<usize>, padre_2: &Vec<usize>, generador: &mut StdRng) -> Vec<usize> {
     let mut descendencia = vec![0; padre_1.len()];
 
+    let cromosomas_a_copiar: Vec<usize> = vec![generador.gen_range(0..padre_1.len()); padre_1.len()/2];
+    /* FIXME mirar si lo de arriba funciona. Si no, usar esto de abajo.
+    for _ in 0 .. cluster.num_elementos/2 {
+        cromosomas.push(generador.gen_range(0..cluster.num_elementos));
+    }
+    */
+
     // Copiar cromosomas del padre 1
-    for i in cromosomas_padre_1.iter() {
+    for i in cromosomas_a_copiar.iter() {
         descendencia[*i] = padre_1[*i];
     }
 
@@ -22,8 +33,37 @@ pub fn cruce_uniforme(padre_1: &Vec<usize>, padre_2: &Vec<usize>, cromosomas_pad
 }
 
 
-pub fn cruce_segmento_fijo(padre_1: &Vec<usize>, padre_2: &Vec<usize>, cromosomas_padre_1: &Vec<usize>) -> Vec<usize> {
+pub fn cruce_segmento_fijo(padre_1: &Vec<usize>, padre_2: &Vec<usize>, generador: &mut StdRng) -> Vec<usize> {
     let mut descendencia = vec![0; padre_1.len()];
 
-    // TODO
+    let inicio_segmento = generador.gen_range(0 .. padre_1.len());            // Inicio del segmento
+    let tamano_segmento = generador.gen_range(0.. padre_1.len());
+
+    let mut i = inicio_segmento;
+    let mut copias: usize = 0;
+
+    while copias < tamano_segmento {
+        descendencia[i] = padre_1[i];
+
+        i = (i+1)%padre_1.len();
+        copias = copias + 1;
+    }
+
+    // Mezclar el vector resultante que se queda entre medias de forma aleatoria como hacemos en el cruce uniforme
+    let inicio = min((inicio_segmento + 1)%padre_1.len(), (inicio_segmento + tamano_segmento + 1)%padre_1.len());
+    let fin = max((inicio_segmento + 1)%padre_1.len(), (inicio_segmento + tamano_segmento + 1)%padre_1.len());
+
+    let cromosomas_a_copiar = vec![generador.gen_range(inicio..=fin); fin - inicio + 1];
+
+    for i in cromosomas_a_copiar.iter() {
+        descendencia[*i] = padre_1[*i];
+    }
+
+    for i in inicio ..= fin {
+        if descendencia[i] != 0 {
+            descendencia[i] = padre_2[i];
+        }
+    }
+
+    descendencia
 }
